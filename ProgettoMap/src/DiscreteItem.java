8273502c76d01025e4bb0f8e9f900204c965221e
Fabio <f.falcone21@studenti.uniba.it>
2023-05-02 17:43:50 +0200

    start secondo sprint

---
+++ ProgettoMap/src/DiscreteItem.java
@@ -0,0 +1,15 @@
+
+public class DiscreteItem extends Item {
+
+    DiscreteItem(DiscreteAttribute attribute, String value) {
+        super(attribute, value);
+    }
+
+    double distance(Object a) {
+        
+        if (getValue().equals(a)) {
+            return 0;
+        } else return 1;
+        
+    }
+}
